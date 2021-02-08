73dad66cd9f13e5ebe99a3621c33882834c8c94a
niksolid <semenov.send@gmail.com>
2021-02-08 18:49:32 +0300

    подключает react-router-dom, прописывает маршруты, добавляет модуль со страницей 404, реализует в компанентах перелинковку на главную страницу при клике на лого

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Main from '../main/main';
-// import Login from '../login/login';
-// import Favorites from '../favorites/favorites';
-// import Room from '../property/property';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 
-const App = ({cardsQuantity}) => <Main cardsQuantity={cardsQuantity} />;
-// const App = () => <Login />;
-// const App = () => <Favorites />;
-// const App = () => <Room />;
+import Main from '../main/main';
+import Login from '../login/login';
+import Favorites from '../favorites/favorites';
+import Room from '../room/room';
+import NotFound from '../not-found/not-found';
+
+const App = ({cardsQuantity}) => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        <Main cardsQuantity={cardsQuantity} />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/favorites">
+        <Favorites />
+      </Route>
+      <Route exact path="/offer/:id?">
+        <Room/>
+      </Route>
+      <Route >
+        <NotFound />
+      </Route>
+    </Switch>
+  </BrowserRouter>
+);
 
 App.propTypes = {
   cardsQuantity: PropTypes.array.isRequired,
